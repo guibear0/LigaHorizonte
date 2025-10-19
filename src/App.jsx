05c3ffea86cdc95884de636595e8719a1225ec9a
Guille <guibear0@gmail.com>
2025-10-19 16:34:59 +0200

    comentario

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Landing from "./pages/Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Competition from "./pages/Competition";
-//dddd
+
 export default function App() {
   return (
     <BrowserRouter>
